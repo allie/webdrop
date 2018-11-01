5fde0dd49042fafb753575ed3f773e354ec9c8a1
allie <littlestcake@gmail.com>
2018-10-31 22:16:27 -0400

    Add frequency band levels as data points, including their totals, averages, normalized averages, and relative averages

--- src/js/visualizer.js
+++ src/js/visualizer.js
@@ -1,6 +1,6 @@
 export class Visualizer {
 	constructor(source, canvas) {
-		this.sampleCount = 512;
+		this.sampleCount = 1024;
 
 		// Web audio nodes
 		this.source = source;
@@ -47,14 +47,25 @@ export class Visualizer {
 				l: new Float32Array(this.sampleCount),
 				r: new Float32Array(this.sampleCount)
 			},
-			bass: 0.0,
-			bassAttenuated: 0.0,
-			mid: 0.0,
-			midAttenuated: 0.0,
-			treble: 0.0,
-			trebleAttenuated: 0.0,
-			volume: 0.0,
-			volumeAttenuated: 0.0
+			bass: {
+				total: 0,
+				average: 0,
+				normalized: 0,
+				relative: 0
+			},
+			mid: {
+				total: 0,
+				average: 0,
+				normalized: 0,
+				relative: 0
+			},
+			treble: {
+				total: 0,
+				average: 0,
+				normalized: 0,
+				relative: 0
+			},
+			volume: 0
 		};
 
 		// FPS data
@@ -96,6 +107,65 @@ export class Visualizer {
 		this.soundData.freqNormalized.mix = this.soundData.freq.mix.map(normalize);
 		this.soundData.freqNormalized.l = this.soundData.freq.l.map(normalize);
 		this.soundData.freqNormalized.r = this.soundData.freq.r.map(normalize);
+
+		// Calculate the frequency range for each of 3 frequency bands
+		let range = {
+			min: 100,
+			max: this.audioCtx.sampleRate / 4
+		};
+
+		let octaves = Math.log(range.max / range.min) / Math.log(2);
+		let octavesPerBand = octaves / 3;
+
+		let bassStart = range.min;
+		let midStart = range.min * Math.pow(2, octavesPerBand);
+		let trebleStart = range.min * Math.pow(2, octavesPerBand * 2);
+
+		let bass = [];
+		let mid = [];
+		let treble = [];
+
+		// Divide audio into bass, mid, and treble
+		for (let i = 0; i < this.sampleCount / 2; i++) {
+			let hz = i * (range.max / (this.sampleCount / 2));
+
+			if (hz >= bassStart && hz < midStart) {
+				bass.push(this.soundData.freq.mix[i]);
+			}
+
+			else if (hz >= midStart && hz < trebleStart) {
+				mid.push(this.soundData.freq.mix[i]);
+			}
+
+			else if (hz >= trebleStart) {
+				treble.push(this.soundData.freq.mix[i]);
+			}
+		}
+
+		// Calculate total and average levels
+		let sum = (total, val) => {
+			return total + val;
+		};
+		this.soundData.bass.total = bass.reduce(sum);
+		this.soundData.bass.average = this.soundData.bass.total / bass.length;
+		this.soundData.mid.total = mid.reduce(sum);
+		this.soundData.mid.average = this.soundData.mid.total / mid.length;
+		this.soundData.treble.total = treble.reduce(sum);
+		this.soundData.treble.average = this.soundData.treble.total / treble.length;
+
+		// Average levels relative to one another; [0, 1]
+		let norm = {
+			min: Math.min(this.soundData.bass.average, this.soundData.mid.average, this.soundData.treble.average),
+			max: Math.max(this.soundData.bass.average, this.soundData.mid.average, this.soundData.treble.average)
+		};
+		this.soundData.bass.relative = (this.soundData.bass.average - norm.min) / (norm.max - norm.min);
+		this.soundData.mid.relative = (this.soundData.mid.average - norm.min) / (norm.max - norm.min);
+		this.soundData.treble.relative = (this.soundData.treble.average - norm.min) / (norm.max - norm.min);
+
+		// Average levels normalized by minimum and maximum decibels; [0, 1]
+		this.soundData.bass.normalized = normalize(this.soundData.bass.average);
+		this.soundData.mid.normalized = normalize(this.soundData.mid.average);
+		this.soundData.treble.normalized = normalize(this.soundData.treble.average);
 	}
 
 	nextFrame() {
